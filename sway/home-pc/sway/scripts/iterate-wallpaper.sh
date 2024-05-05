9db721b2c6d0d14e4e6fba2c64245c8375a23c11
Aleix Soler <aleixsoler64@gmail.com>
2024-05-05 18:41:03 +0200

    chore: init office branch

--- sway/home-pc/sway/scripts/iterate-wallpaper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wallpaper_dir=$HOME/Pictures/Wallpapers
-wallpapers=($(ls -1 $wallpaper_dir))
-next_wall=$((0 + $RANDOM % ${#wallpapers[@]}))
-swaymsg "output * bg $wallpaper_dir/${wallpapers[$next_wall]} fill"
