b6441a71108a8d0092b5f37b07d5ad1b2598320f
Aleix Soler <aleixsoler64@gmail.com>
2024-10-13 17:56:04 +0200

    feat: add configuration for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,11 @@ local wezterm = require "wezterm"
 local config = {}
 
 
-config.enable_scroll_bar = true;
+config.enable_scroll_bar = true
+config.window_background_opacity = 0.88
+
+config.hide_tab_bar_if_only_one_tab = true;
+
 
 config.font = wezterm.font 'JetBrains Mono'
 
