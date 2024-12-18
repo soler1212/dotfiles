27e784f82a07942536a1fd361096f3e9c857cf1b
Aleix Soler <aleixsoler64@gmail.com>
2024-12-18 19:22:47 +0100

    feat: override the ui select for telescope

--- nvim/lua/cargolreactiu/after/plugins/telescope.lua
+++ nvim/lua/cargolreactiu/after/plugins/telescope.lua
@@ -7,3 +7,32 @@ vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Find Git files' }
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+
+-- INFO: https://github.com/nvim-telescope/telescope-ui-select.nvim
+require("telescope").setup {
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {
+        -- even more opts
+      }
+
+      -- pseudo code / specification for writing custom displays, like the one
+      -- for "codeactions"
+      -- specific_opts = {
+      --   [kind] = {
+      --     make_indexed = function(items) -> indexed_items, width,
+      --     make_displayer = function(widths) -> displayer
+      --     make_display = function(displayer) -> function(e)
+      --     make_ordinal = function(e) -> string
+      --   },
+      --   -- for example to disable the custom builtin "codeactions" display
+      --      do the following
+      --   codeactions = false,
+      -- }
+    }
+  }
+}
+-- To get ui-select loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require("telescope").load_extension("ui-select")
--- nvim/lua/cargolreactiu/plugins/telescope.lua
+++ nvim/lua/cargolreactiu/plugins/telescope.lua
@@ -1,6 +1,13 @@
 -- plugins/telescope.lua:
-return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+return { {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' }
+},
+
+  -- It sets vim.ui.select to telescope. That means for example that neovim core stuff can fill the telescope picker.
+  -- https://github.com/nvim-telescope/telescope-ui-select.nvim
+  {
+    "nvim-telescope/telescope-ui-select.nvim"
+  }
 }
