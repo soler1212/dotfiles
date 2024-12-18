cd8a5a32acf354b7cff3c43c4dd2643c3110b828
Aleix Soler <aleixsoler64@gmail.com>
2024-12-18 19:50:25 +0100

    feat: add gitsigns

--- nvim/lua/cargolreactiu/after/plugins/which_key.lua
+++ nvim/lua/cargolreactiu/after/plugins/which_key.lua
@@ -18,6 +18,9 @@ wk.add({
   { "<leader>bc", group = "close" }, -- group
   { "<leader>s", group = "Session" }, -- group
   { "<leader>sd", group = "Delete"}, -- group
+  { "<leader>g", group = "Git"}, -- group
+  { "<leader>gh", group = "Hunk"}, -- group
+  { "<leader>gt", group = "Toggle"}, -- group
   {
     mode = { "n", "v" }, -- NORMAL and VISUAL mode
     { "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
---
+++ nvim/lua/cargolreactiu/plugins/gitsigns.lua
@@ -0,0 +1,5 @@
+-- Git integration for buffers
+-- https://github.com/lewis6991/gitsigns.nvim
+return {
+  "lewis6991/gitsigns.nvim"
+}
