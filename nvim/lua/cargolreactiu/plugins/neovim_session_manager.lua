b18114ef8bd2de8e72299c922ca26c5670f719f8
Aleix Soler <aleixsoler64@gmail.com>
2024-12-29 19:38:48 +0100

    chore: remove ols session manager plugin

--- nvim/lua/cargolreactiu/plugins/neovim_session_manager.lua
+++
@@ -1,7 +0,0 @@
--- A simple wrapper around :mksession.
--- https://github.com/Shatur/neovim-session-manager
-return {
-  "Shatur/neovim-session-manager",
-  event = "User BaseDefered",
-  cmd = "SessionManager",
-}
