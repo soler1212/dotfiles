af964007e870f10db571efe52063aa3692a42a84
Aleix Soler <aleixsoler@blueroominnovation.com>
2024-12-30 07:33:02 +0100

    chore: ensure some ls installation

--- nvim/lua/cargolreactiu/after/plugins/lsp.lua
+++ nvim/lua/cargolreactiu/after/plugins/lsp.lua
@@ -95,7 +95,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = {'lua_ls' },
+  ensure_installed = {'lua_ls', 'ts_ls', 'eslint', 'anakin_language_server' },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
