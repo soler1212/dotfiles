64ea1d4d97aa9f151b713cdcee0e094b7cf59c41
Aleix Soler <aleixsoler64@gmail.com>
2025-05-18 19:25:29 +0200

    chore: neotree groups

--- nvim/lua/cargolreactiu/after/plugins/which_key.lua
+++ nvim/lua/cargolreactiu/after/plugins/which_key.lua
@@ -10,38 +10,61 @@ local opts = {
 
 -- Define custom keybindings
 wk.add({
-  { "<leader>f", group = "file" }, -- group
-  { "<leader>fo", group = "other" }, -- group
-  { "<leader>b", group = "buffer" }, -- group
-  { "<leader>bg", group = "go to" }, -- group
-  { "<leader>bm", group = "move" }, -- group
-  { "<leader>bo", group = "order" }, -- group
-  { "<leader>bc", group = "close" }, -- group
-  { "<leader>s", group = "Session" }, -- group
-  { "<leader>sd", group = "Delete"}, -- group
-  { "<leader>g", group = "Git"}, -- group
-  { "<leader>gt", group = "Toggle"}, -- group
-  { "<leader>m", group = "Mark"}, -- group
-  { "<leader>mm", desc = "Toggle mark"}, -- group
-  { "<leader>mc", desc = "Clear all marks in current buffer"}, -- group
-  { "<leader>mp", desc = "Preview mark content"}, -- group
-  { "<leader>md", desc = "Delete a letter mark"}, -- group
-  { "<leader>ms", desc = "Set a letter mark"}, -- group
-  { "<leader>ml", desc = "List marks"}, -- group
-  { "<leader>mt", desc = "Todo Comments"}, -- group
-  { "<leader>mlb", desc = "List Marks in current buffer"}, -- group
-  { "<leader>mla", desc = "List Marks in all buffers"}, -- group
-  { "<leader>l", desc = "LSP"}, -- group
-  { "<leader>ls", desc = "Saga"},
-  { "<leader>t", desc = "Tabs"},
-  { "<leader>lM", vim.diagnostic.goto_prev, desc = "Go to previous diagnostic [M]essage"},
-  { "<leader>lm", vim.diagnostic.goto_next, desc = "Go to next diagnostic [M]essage"},
-  { "<leader>ld", vim.diagnostic.open_float, desc = "Show diagnostic [E]rror messages"},
-  { "<leader>lq", vim.diagnostic.setloclist, desc = "Open diagnostic [Q]uickfix list"},
-  { "<leader>lt", desc = "Trouble"}, -- group
-  {
-    mode = { "n", "v" }, -- NORMAL and VISUAL mode
-    { "<leader>q", "<cmd>qa<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
-  },
+  -- File keymaps
+  { "<leader>f",   { name = "file" } },
+  { "<leader>fo",  { name = "other" } },
+
+  -- Buffer keymaps
+  { "<leader>b",   { name = "buffer" } },
+  { "<leader>bg",  { name = "go to" } },
+  { "<leader>bm",  { name = "move" } },
+  { "<leader>bo",  { name = "order" } },
+  { "<leader>bc",  { name = "close" } },
+
+  -- Session keymaps
+  { "<leader>s",   { name = "Session" } },
+  { "<leader>sd",  { name = "Delete" } },
+
+  -- Git keymaps
+  { "<leader>g",   { name = "Git" } },
+  { "<leader>gt",  { name = "Toggle" } },
+  { "<leader>gs",  { desc = "Stage current hunk" } },
+  { "<leader>gr",  { desc = "Reset current hunk" } },
+  { "<leader>gS",  { desc = "Stage all changes in buffer" } },
+  { "<leader>gu",  { desc = "Undo last staged hunk" } },
+  { "<leader>gR",  { desc = "Reset all changes in buffer" } },
+  { "<leader>gp",  { desc = "Preview current hunk" } },
+  { "<leader>gb",  { desc = "Show detailed blame for current line" } },
+  { "<leader>gd",  { desc = "Show diff against index" } },
+  { "<leader>gD",  { desc = "Show diff against previous commit" } },
+  { "<leader>gtl", { desc = "Toggle inline blame annotations" } },
+  { "<leader>gtx", { desc = "Toggle display of deleted lines" } },
+
+
+  -- Mark keymaps
+  { "<leader>m",   { name = "Mark" } },
+  { "<leader>mm",  desc = "Toggle mark" },
+  { "<leader>mc",  desc = "Clear all marks in current buffer" },
+  { "<leader>mp",  desc = "Preview mark content" },
+  { "<leader>md",  desc = "Delete a letter mark" },
+  { "<leader>ms",  desc = "Set a letter mark" },
+  { "<leader>ml",  desc = "List marks" },
+  { "<leader>mt",  desc = "Todo Comments" },
+  { "<leader>mlb", desc = "List Marks in current buffer" },
+  { "<leader>mla", desc = "List Marks in all buffers" },
+
+
+  -- Tabs keymaps
+  { "<leader>t",   { name = "Tabs" } },
+
+  -- LSP keymaps
+  { "<leader>l",   { name = "LSP" } },
+  { "<leader>ls",  { name = "Saga" } },
+  { "<leader>lt",  { name = "Trouble" } },
+  { "<leader>lM",  { desc = "Go to previous diagnostic [M]essage" } },
+  { "<leader>lm",  { desc = "Go to next diagnostic [M]essage" } },
+  { "<leader>ld",  { desc = "Show diagnostic [E]rror messages" } },
+  { "<leader>lq",  { desc = "Open diagnostic [Q]uickfix list" } },
 })
+
 wk.setup(opts)
