b2b5a1f06d37ff4762ff76d657c8c7d18a8fd2aa
Aleix Soler <aleixsoler64@gmail.com>
2026-04-07 07:27:49 +0200

    chore: clean flake

--- ags/flake.nix
+++ ags/flake.nix
@@ -44,7 +44,6 @@
       ++ [
         pkgs.libadwaita
         pkgs.libsoup_3
-        pkgs.webkitgtk_6_0
       ];
   in {
     packages.${system} = {
@@ -65,15 +64,8 @@
 
           mkdir -p $out/bin
           mkdir -p $out/share
-          # Copy only relevant files to avoid including node_modules if possible
-          # but ags bundle might need them. Let's copy everything except .git and node_modules
-          # if we want to be clean, but for now let's just ensure it works.
-          cp -r moon $out/share/
-          
-          # ags bundle entry output -d "SRC='...'"
-          # We need to be in the directory where app.ts can resolve its imports
-          cd $out/share
-          ags bundle ${entry} $out/bin/${pname}
+          cp -r * $out/share
+          ags bundle ${entry} $out/bin/${pname} -d "SRC='$out/share'"
 
           runHook postInstall
         '';
